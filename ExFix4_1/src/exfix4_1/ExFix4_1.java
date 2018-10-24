0f5434426f0ea912f27e5a43caee365edc043d5a
Cristian Araujo de Jesus <araujocristian@live.com>
2018-10-24 13:53:30 -0300

    Add ExFix4.1

---
+++ ExFix4_1/src/exfix4_1/ExFix4_1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exfix4_1;
+
+/**
+ *
+ * @author ime
+ */
+public class ExFix4_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Circulo cir1 = new Circulo(5);
+        
+        System.out.println(cir1.area());
+        System.out.println(cir1.perimetro());
+    }
+    
+}
