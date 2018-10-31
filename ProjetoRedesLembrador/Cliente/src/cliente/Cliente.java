7a14f869c099354ab273b4e517734334a953c427
Cristian Araujo <araujocristian@live.com>
2018-10-31 20:28:01 -0300

    update Cliente code

--- ProjetoRedesLembrador/Cliente/src/cliente/Cliente.java
+++ ProjetoRedesLembrador/Cliente/src/cliente/Cliente.java
@@ -3,6 +3,7 @@ package cliente;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
@@ -12,31 +13,55 @@ import javax.swing.JOptionPane;
  * @author cristian
  */
 public class Cliente {
+    
     public static void main(String[] args) {
         try {
             Socket cliente = new Socket("127.0.0.1",12345);
+            // cria variaveis de entrada e saida de dados
             ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
             ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
             
-            // PEGAR DADOS DO LEMBRETE
+            // array de lembretes
+            ArrayList<Lembrete> lembretes = new ArrayList<>();
+            
+            int cont = 0;
+            
+            // pega dados dos lembretes
             Scanner teclado = new Scanner(System.in);
-            
-            String s;
+            int n;
+            String nome;
             String hora;
-
-            System.out.printf("Informe o nome do Lembrete:\n");
-            s = teclado.nextLine();
-
-            System.out.printf("Informe a hora do Lembrete no formato HH:mm:ss :\n");
-            hora = teclado.next();
             
-            Lembrete lembrete = new Lembrete(s,hora);
+            System.out.println("Informe a quantidade de lembretes: ");
+            n = teclado.nextInt();
             
-            saida.flush();
-            saida.writeObject(hora);
+            for(int j=0; j < n; j++){
+                System.out.println("Informe o nome do Lembrete: ");
+                nome = teclado.nextLine();
 
-            String horaLembrete = (String) entrada.readObject();
-            JOptionPane.showMessageDialog(null,"Lembrar de " + lembrete.getNome());
+                System.out.printf("Informe a hora do Lembrete no formato HH:mm:ss :\n");
+                hora = teclado.next();
+
+                Lembrete lembrete = new Lembrete(nome,hora);
+                lembretes.add(lembrete);
+                
+                // envia dados para o servidor
+                saida.flush();
+                saida.writeObject(hora);
+            }
+            
+            // verifica entrada e esperar para lembrar
+            while(cont < n){
+                String horaLembrete = (String) entrada.readObject();
+
+                for(Lembrete lem: lembretes){
+                    if(lem.getHora().equals(horaLembrete)){
+                        cont++;
+                        JOptionPane.showMessageDialog(null,"Lembrar de " + lem.getNome());
+                    }
+                }
+            }
+            
             entrada.close();
             System.out.println("Conexão encerrada");
         }
