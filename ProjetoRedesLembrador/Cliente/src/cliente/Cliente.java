66fcd7871741eb2f30d87daf4811ff3274282030
Cristian Araujo <araujocristian@live.com>
2018-10-31 22:57:30 -0300

    Add telas do cliente e servidor

--- ProjetoRedesLembrador/Cliente/src/cliente/Cliente.java
+++ ProjetoRedesLembrador/Cliente/src/cliente/Cliente.java
@@ -6,8 +6,6 @@ import java.net.Socket;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -21,13 +19,18 @@ public class Cliente extends JFrame {
     
     private static JTextField txtIP;
     private static JTextField txtPorta;
+    private static JTextField txtQuant;
     private static JTextField txtNome;
+    private static JTextField txtHora;
+    private static String nome;
+    private static String hora;
+    private static int n;
 
     
     public static void main(String[] args) {
         try {
             
-            JLabel lblMessage = new JLabel("Verificar!");
+            JLabel lblMessage = new JLabel("Conectar ao Servidor:");
             txtIP = new JTextField("127.0.0.1");
             txtPorta = new JTextField("12345");
             Object[] texts = {lblMessage, txtIP, txtPorta };
@@ -46,22 +49,23 @@ public class Cliente extends JFrame {
             int cont = 0;
             
             // pega dados dos lembretes
-            Scanner teclado = new Scanner(System.in);
-            int n;
-            String nome;
-            String hora;
             
-            System.out.println("Informe a quantidade de lembretes: ");
-            n = teclado.nextInt();
-            teclado.nextLine(); // esvazia o buffer do teclado
+            JLabel lblMessage2 = new JLabel("Informe a quantidade de lembretes:");
+            txtQuant = new JTextField("2");
+            Object[] texts2 = {lblMessage2, txtQuant};
+            JOptionPane.showMessageDialog(null, texts2);
+
+            n = Integer.parseInt(txtQuant.getText());
 
             for(int j=0; j < n; j++){
-                System.out.println("Informe o nome do Lembrete: ");
-                nome = teclado.nextLine();
+                JLabel lblMessage3 = new JLabel("Informe dados do lembrete:");
+                txtNome = new JTextField("Beber água");
+                txtHora = new JTextField("22:48");
+                Object[] texts3 = {lblMessage3, txtNome, txtHora};
+                JOptionPane.showMessageDialog(null, texts3);
 
-                System.out.printf("Informe a hora do Lembrete no formato HH:mm:ss :\n");
-                hora = teclado.next();
-                teclado.nextLine(); // esvazia o buffer do teclado
+                nome = txtNome.getText();
+                hora = txtHora.getText();
                 
                 // cria o localTime formatado
                 DateTimeFormatter dtf = DateTimeFormatter.ISO_LOCAL_TIME;
@@ -70,7 +74,7 @@ public class Cliente extends JFrame {
                 // cria o lembrete
                 Lembrete lembrete = new Lembrete(nome,horaLembrete);
                 lembretes.add(lembrete);
-                System.out.println("\n Lembrete Adicionado! \n");
+                System.out.println("\n Lembrete Adicionado!");
                 
                 // envia dados para o servidor
                 saida.flush();
@@ -96,7 +100,7 @@ public class Cliente extends JFrame {
             // encerra sessão
             entrada.close();
             cliente.close();
-            System.out.println("Conexão encerrada");
+            System.out.println("Agradecemos por usar o Lembrador!");
         }
         catch(Exception e) {
           System.out.println("Erro: " + e.getMessage());
--- ProjetoRedesLembrador/Servidor/src/servidor/Servidor.java
+++ ProjetoRedesLembrador/Servidor/src/servidor/Servidor.java
@@ -5,6 +5,9 @@ import java.io.ObjectOutputStream;
 import java.net.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -12,52 +15,61 @@ import java.util.ArrayList;
  */
 public class Servidor {
     private static final Lembrete LEMBRETE = new Lembrete();
+    
+    private static JTextField txtPorta;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
     try {
-      // Instancia o ServerSocket ouvindo a porta 12345
-      ServerSocket servidor = new ServerSocket(12345);
-      System.out.println("Servidor ouvindo a porta 12345");
+        
+        
+        JLabel lblMessage = new JLabel("Abrir porta do Servidor:");
+        txtPorta = new JTextField("12345");
+        Object[] texts = {lblMessage, txtPorta };
+        JOptionPane.showMessageDialog(null, texts);
+        
+        // Instancia o ServerSocket ouvindo a porta da entrada
+        ServerSocket servidor = new ServerSocket(Integer.parseInt(txtPorta.getText()));
+        System.out.println("Servidor ouvindo a porta: " + Integer.parseInt(txtPorta.getText()));
       
-      while(true) {
-        // o método accept() bloqueia a execução até que o servidor receba um pedido de conexão
-        Socket cliente = servidor.accept();
-        System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
-        
-        // cria variaveis de entrada e saida
-        ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-        ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-        
-        // cria Array de horas de lembretes
-        ArrayList<LocalTime> horaLembretes = new ArrayList<>();
-        String clienteDado = "";
-        
-        // armazena dados do cliente
-        while(true){            
-            clienteDado = (String) entrada.readObject();
-            
-            if(clienteDado.equals("f")) break;
+        while(true) {
+            // o método accept() bloqueia a execução até que o servidor receba um pedido de conexão
+            Socket cliente = servidor.accept();
+            System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
 
-            LocalTime horaLembrete = LEMBRETE.formatarHora(clienteDado);
-            
-            horaLembretes.add(horaLembrete);
-            LEMBRETE.textoAlarmeAdd(horaLembrete);
-        }
-        
-        while(true){
-            for(LocalTime h: horaLembretes){
-                if(LEMBRETE.alarmar(h)){
-                    saida.flush();
-                    saida.writeObject(h);
-                    horaLembretes.remove(h);
-                }
+            // cria variaveis de entrada e saida
+            ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+            ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+
+            // cria Array de horas de lembretes
+            ArrayList<LocalTime> horaLembretes = new ArrayList<>();
+            String clienteDado = "";
+
+            // armazena dados do cliente
+            while(true){            
+                clienteDado = (String) entrada.readObject();
+
+                if(clienteDado.equals("f")) break;
+
+                LocalTime horaLembrete = LEMBRETE.formatarHora(clienteDado);
+
+                horaLembretes.add(horaLembrete);
+                LEMBRETE.textoAlarmeAdd(horaLembrete);
             }
-            
-            if(horaLembretes.isEmpty()) saida.close();
-        }
-      }  
+
+            while(true){
+                for(LocalTime h: horaLembretes){
+                    if(LEMBRETE.alarmar(h)){
+                        saida.flush();
+                        saida.writeObject(h);
+                        horaLembretes.remove(h);
+                    }
+                }
+
+                if(horaLembretes.isEmpty()) saida.close();
+            }
+         }  
     }   
     catch(Exception e) {
        System.out.println("Erro: " + e.getMessage());
